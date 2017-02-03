a77699aeb5640b208bef1fa7a38d82b14a849e64
ericb <eric_bastarache@hotmail.com>
2017-02-03 17:25:53 -0500

    Added preliminary code to highlight words

--- js/highlighter.js
+++ js/highlighter.js
@@ -1,16 +1,42 @@
 const newWordBtn = document.getElementById('add-word');
+const highlightBtn = document.getElementById('highlight-btn');
 const wordField = document.getElementById('word-to-add');
 var wordArr = [];
+
 newWordBtn.addEventListener('click', () => {
   wordArr.push(wordField.value);
   wordField.value = "";
 
-  for(var i = 0; i < wordArr.length; i++) {
-    console.log(wordArr[i]);
+  for(let i = 0; i < wordArr.length; i++) {
+    //console.log(wordArr[i]);
     var wordItem = document.createElement('li');
     wordItem.innerHTML = wordArr[i];
     var listOfWords = document.getElementById('words-list');
     listOfWords.append(wordItem);
   }
   wordArr.pop();
+});
+
+wordField.addEventListener('keydown', (e) => {
+  if(e.code === "Enter") {
+    e.preventDefault();
+    wordArr.push(wordField.value);
+    wordField.value = "";
+  }
+  for(let i = 0; i < wordArr.length; i++) {
+    //console.log(wordArr[i]);
+    var wordItem = document.createElement('li');
+    wordItem.innerHTML = wordArr[i];
+    var listOfWords = document.getElementById('words-list');
+    listOfWords.append(wordItem);
+  }
+  wordArr.pop();
+})
+
+highlightBtn.addEventListener('click', () => {
+  var words = document.getElementsByTagName('li');
+  for(let wd = 0; wd < words.length; wd++) {
+    //console.log(words[wd].innerHTML);
+    
+  }
 });
