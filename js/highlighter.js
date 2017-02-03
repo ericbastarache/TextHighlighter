93774abd0d3d8ba2940a7a4006c972de0c8e17d6
ericb <eric_bastarache@hotmail.com>
2017-02-02 23:12:20 -0500

    Initial commit

---
+++ js/highlighter.js
@@ -0,0 +1,15 @@
+const newWordBtn = document.getElementById('add-word');
+const wordField = document.getElementById('word-to-add');
+var wordArr = [];
+newWordBtn.addEventListener('click', () => {
+  wordArr.push(wordField.value);
+  wordField.value = "";
+
+  for(var i = 0; i < wordArr.length; i++) {
+    console.log(wordArr[i]);
+    var wordItem = document.createElement('li');
+    wordItem.innerHTML = wordArr[i];
+    var listOfWords = document.getElementById('words-list');
+    listOfWords.append(wordItem);
+  }
+});
