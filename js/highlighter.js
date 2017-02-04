b07d1666283823c1cad88df26ba485f1d5b1509d
ericb <eric_bastarache@hotmail.com>
2017-02-03 19:52:41 -0500

    added WIP highlighting code

--- js/highlighter.js
+++ js/highlighter.js
@@ -1,6 +1,8 @@
 const newWordBtn = document.getElementById('add-word');
 const highlightBtn = document.getElementById('highlight-btn');
 const wordField = document.getElementById('word-to-add');
+const error = document.getElementById('error');
+const toHighlight = document.getElementById('highlight-me');
 var wordArr = [];
 
 newWordBtn.addEventListener('click', () => {
@@ -8,7 +10,7 @@ newWordBtn.addEventListener('click', () => {
     wordArr.push(wordField.value);
     wordField.value = "";
   }
-  
+
   for(let i = 0; i < wordArr.length; i++) {
     //console.log(wordArr[i]);
     var wordItem = document.createElement('li');
@@ -39,11 +41,26 @@ wordField.addEventListener('keydown', (e) => {
 })
 
 highlightBtn.addEventListener('click', () => {
-  var words = document.getElementsByTagName('li');
   var highlightThese = [];
+
+  var words = document.getElementsByTagName('li');
   for(let wd = 0; wd < words.length; wd++) {
     //console.log(words[wd].innerHTML);
     highlightThese.push(words[wd].innerHTML);
-    console.log(highlightThese);
+
+    if(highlightThese.length >= 1) {
+      error.style = "display: block; color: #20d037;";
+      error.innerHTML = "Thank you for adding words to highlight.";
+    } else {
+      error.style = "display: block;";
+    }
+  }
+  for(let hlWd = 0; hlWd < highlightThese.length; hlWd++) {
+    //console.log(highlightThese[hlWd]);
+    if(~toHighlight.innerHTML.indexOf(highlightThese[hlWd])) {
+      console.log(highlightThese[hlWd].length);
+      toHighlight.innerHTML.replace(highlightThese[hlWd], `<span class='highlight'>${highlightThese[hlWd]}</span>`);
+      console.log(toHighlight.innerHTML);
+    }
   }
 });
